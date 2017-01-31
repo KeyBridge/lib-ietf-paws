a58a6e296b23ce9ddfd562abaf1a7db5e29b4830
Jesse Caulfield <jesse.caulfield@keybridgeglobal.com>
2017-01-30 22:44:35 -0500

    add version container

---
+++ src/main/java/ch/keybridge/lib/paws/Version.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2017 Key Bridge LLC. All rights reserved.
+ * Use is subject to license terms.
+ *
+ * Software Code is protected by Copyrights. Author hereby reserves all rights
+ * in and to Copyrights and no license is granted under Copyrights in this
+ * Software License Agreement.
+ *
+ * Key Bridge LLC generally licenses Copyrights for commercialization pursuant to
+ * the terms of either a Standard Software Source Code License Agreement or a
+ * Standard Product License Agreement. A copy of either Agreement can be
+ * obtained upon request from: info@keybridgewireless.com
+ */
+package ch.keybridge.lib.paws;
+
+/**
+ * Version container.
+ *
+ * @author Key Bridge LLC
+ */
+public class Version {
+
+  public static final String VERSION = "0.1.0";
+
+}
