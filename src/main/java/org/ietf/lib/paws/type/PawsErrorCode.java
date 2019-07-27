bcfbafdb850b4567e267ebe0bc919740125ed3f1
Jesse Caulfield <jesse.caulfield@keybridgewireless.com>
2019-07-27 10:15:05 -0400

    correct paws error codes

--- src/main/java/org/ietf/lib/paws/type/PawsErrorCode.java
+++ src/main/java/org/ietf/lib/paws/type/PawsErrorCode.java
@@ -14,6 +14,7 @@
 package org.ietf.lib.paws.type;
 
 import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
 import javax.xml.bind.annotation.XmlType;
 
 /**
@@ -37,6 +38,7 @@ public enum PawsErrorCode {
    * The Database does not support the specified version of the message. This
    * error does not use any additional data.
    */
+  @XmlEnumValue("-101")
   VERSION(-101, "The Database does not support the specified version of the message."),
   /**
    * The Database does not support the device. For example, it supports none of
@@ -44,11 +46,13 @@ public enum PawsErrorCode {
    * on its device type, model, etc. This error does not use any additional
    * data.
    */
+  @XmlEnumValue("-102")
   UNSUPPORTED(-102, "The Database does not support the device."),
   /**
    * The Database does not implement the optional request or optional feature.
    * This error does not use any additional data.
    */
+  @XmlEnumValue("-103")
   UNIMPLEMENTED(-103, "The Database does not implement the optional request or optional feature."),
   /**
    * The specified geolocation is outside the coverage area of the Database. The
@@ -57,6 +61,7 @@ public enum PawsErrorCode {
    * location. See OUTSIDE_COVERAGE Error (Section 5.17.1 of [RFC7545]) for more
    * details.
    */
+  @XmlEnumValue("-104")
   OUTSIDE_COVERAGE(-104, "The specified geolocation is outside the coverage area of the Database."),
   /**
    * The Database has changed its URI. The Database MAY include a DbUpdateSpec
@@ -66,6 +71,7 @@ public enum PawsErrorCode {
    * databases listed in the DbUpdateSpec. See DATABASE_CHANGE Error (Section
    * 5.17.2 of [RFC7545]) for more details.
    */
+  @XmlEnumValue("-105")
   DATABASE_CHANGE(-105, "The Database has changed its URI."),
 
   /**
@@ -75,12 +81,14 @@ public enum PawsErrorCode {
    * missing parameters may reduce the number of re-queries from the device. See
    * MISSING Error (Section 5.17.3 of [RFC7545]) for more details.
    */
+  @XmlEnumValue("-201")
   MISSING(-201, "A required parameter is missing."),
   /**
    * A parameter value is invalid in some way. The Database SHOULD include a
    * message indicating which parameter and why its value is invalid. This error
    * does not use any additional data.
    */
+  @XmlEnumValue("-202")
   INVALID_VALUE(-202, "A parameter value is invalid in some way."),
 
   /**
@@ -88,29 +96,34 @@ public enum PawsErrorCode {
    * determined by the ruleset or be dependent on prior arrangement between the
    * device and Database. This error does not use any additional data.
    */
+  @XmlEnumValue("-301")
   UNAUTHORIZED(-301, "The device is not authorized to used the Database."),
   /**
    * Device registration required, but the device is not registered. This error
    * does not use any additional data.
    */
+  @XmlEnumValue("-302")
   NOT_REGISTERED(-302, "Device registration required, but the device is not registered."),
 
   /**
    * The request was invalid for some other reason. For example, the API key was
    * invalid.
    */
-  BAD_REQUEST(400, "The request was invalid for some other reason. For example, the API key was invalid."),
+  @XmlEnumValue("-400")
+  BAD_REQUEST(-400, "The request was invalid for some other reason. For example, the API key was invalid."),
   /**
    * The number of queries has exceeded the quota allocated to the project
    * associated with the API key. This may also be triggered if the API key
    * wasn't present in the request.
    */
-  QUOTA(403, "The number of queries has exceeded the API key quota. This may also be triggered if the API key wasn't present in the request. "),
+  @XmlEnumValue("-403")
+  QUOTA(-403, "The number of queries has exceeded the API key quota. This may also be triggered if the API key wasn't present in the request. "),
 
   /**
    * The request failed due to an internal error in the database.
    */
-  INTERNAL_DATABASE_ERROR(500, "The request failed due to an internal error in the database.");
+  @XmlEnumValue("-500")
+  INTERNAL_DATABASE_ERROR(-500, "The request failed due to an internal error in the database.");
 
   /**
    * The error code.
