609a370f1a0d159526f1a4201b5460988c625236
Jesse Caulfield <jesse.caulfield@keybridgewireless.com>
2019-07-29 14:26:46 -0400

    add device validation messages

---
+++ src/main/java/org/ietf/lib/paws/message/DeviceValidationRequest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2019 Key Bridge. All rights reserved. Use is subject to license
+ * terms.
+ *
+ * This software code is protected by Copyrights and remains the property of
+ * Key Bridge and its suppliers, if any. Key Bridge reserves all rights in and to
+ * Copyrights and no license is granted under Copyrights in this Software
+ * License Agreement.
+ *
+ * Key Bridge generally licenses Copyrights for commercialization pursuant to
+ * the terms of either a Standard Software Source Code License Agreement or a
+ * Standard Product License Agreement. A copy of either Agreement can be
+ * obtained upon request by sending an email to info@keybridgewireless.com.
+ *
+ * All information contained herein is the property of Key Bridge and its
+ * suppliers, if any. The intellectual and technical concepts contained herein
+ * are proprietary.
+ */
+package org.ietf.lib.paws.message;
+
+import java.util.List;
+import javax.xml.bind.annotation.*;
+import org.ietf.lib.paws.DeviceDescriptor;
+
+/**
+ * 4.6.1. DEV_VALID_REQ
+ * <p>
+ * This request is used by a Master Device to determine which Slave Devices are
+ * permitted to operate.
+ * <pre>
+ *   +---------------------------------------------+
+ *   |DEV_VALID_REQ                                |
+ *   +----------------------------------+----------+
+ *   |deviceDescs:list                  | REQUIRED |---+
+ *   |masterDeviceDesc:DeviceDescriptor | OPTIONAL |   |
+ *   +----------------------------------+----------+   |
+ *                                                     V 1..*
+ *                                    +----------------------+
+ *                                    |DeviceDescriptor      |
+ *                                    +----------------------+
+ * </pre>
+ *
+ * @author Key Bridge
+ */
+@XmlRootElement(name = "DEV_VALID_REQ")
+@XmlType(name = "DEV_VALID_REQ")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class DeviceValidationRequest {
+
+  /**
+   * A DeviceDescriptor (Section 5.2) list is REQUIRED; it specifies the list of
+   * Slave Devices that are to be validated.
+   */
+  @XmlElement(required = true)
+  private List<DeviceDescriptor> deviceDescs;
+  /**
+   * The Master Device MAY provide its own descriptor.
+   */
+  private DeviceDescriptor masterDeviceDesc;
+
+  public List<DeviceDescriptor> getDeviceDescs() {
+    return deviceDescs;
+  }
+
+  public void setDeviceDescs(List<DeviceDescriptor> deviceDescs) {
+    this.deviceDescs = deviceDescs;
+  }
+
+  public DeviceDescriptor getMasterDeviceDesc() {
+    return masterDeviceDesc;
+  }
+
+  public void setMasterDeviceDesc(DeviceDescriptor masterDeviceDesc) {
+    this.masterDeviceDesc = masterDeviceDesc;
+  }
+
+}
---
+++ src/main/java/org/ietf/lib/paws/message/DeviceValidationResponse.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2019 Key Bridge. All rights reserved. Use is subject to license
+ * terms.
+ *
+ * This software code is protected by Copyrights and remains the property of
+ * Key Bridge and its suppliers, if any. Key Bridge reserves all rights in and to
+ * Copyrights and no license is granted under Copyrights in this Software
+ * License Agreement.
+ *
+ * Key Bridge generally licenses Copyrights for commercialization pursuant to
+ * the terms of either a Standard Software Source Code License Agreement or a
+ * Standard Product License Agreement. A copy of either Agreement can be
+ * obtained upon request by sending an email to info@keybridgewireless.com.
+ *
+ * All information contained herein is the property of Key Bridge and its
+ * suppliers, if any. The intellectual and technical concepts contained herein
+ * are proprietary.
+ */
+package org.ietf.lib.paws.message;
+
+import java.util.List;
+import javax.xml.bind.annotation.*;
+import org.ietf.lib.paws.DbUpdateSpec;
+import org.ietf.lib.paws.DeviceValidity;
+
+/**
+ * 4.6.2. DEV_VALID_RESP
+ * <pre>
+ *   +---------------------------------------+
+ *   |DEV_VALID_RESP                         |
+ *   +----------------------------+----------+
+ *   |deviceValidities:list       | REQUIRED |----
+ *   |databaseChange:DbUpdateSpec | OPTIONAL |   |
+ *   +----------------------------+----------+   |
+ *                                               V 1..*
+ *                            +---------------------------------------+
+ *                            |DeviceValidity                         |
+ *                            +----------------------------+----------+
+ *                            |deviceDesc:DeviceDescriptor | REQUIRED |
+ *                            |isValid:boolean             | REQUIRED |
+ *                            |reason:string               | OPTIONAL |
+ *                            +----------------------------+----------+
+ * </pre>
+ *
+ * @author Key Bridge
+ */
+@XmlRootElement(name = "DEV_VALID_RESP")
+@XmlType(name = "DEV_VALID_RESP")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class DeviceValidationResponse {
+
+  /**
+   * A DeviceValidities (Section 5.16) list is REQUIRED to report the list of
+   * Slave Devices and whether each listed device is valid. The number of
+   * entries MUST match the number of DeviceDescriptors (Section 5.2) listed in
+   * the DEV_VALID_REQ message.
+   */
+  @XmlElement(required = true)
+  private List<DeviceValidity> deviceValidities;
+  /**
+   * The Database MAY include a DbUpdateSpec (Section 5.7) to notify the device
+   * of a change to the database URI, providing one or more alternate database
+   * URIs. The device needs to update its preconfigured entry for the responding
+   * Database with the alternate Databases listed in the DbUpdateSpec.
+   * <p>
+   */
+  private DbUpdateSpec databaseChange;
+
+  public List<DeviceValidity> getDeviceValidities() {
+    return deviceValidities;
+  }
+
+  public void setDeviceValidities(List<DeviceValidity> deviceValidities) {
+    this.deviceValidities = deviceValidities;
+  }
+
+  public DbUpdateSpec getDatabaseChange() {
+    return databaseChange;
+  }
+
+  public void setDatabaseChange(DbUpdateSpec databaseChange) {
+    this.databaseChange = databaseChange;
+  }
+
+}
